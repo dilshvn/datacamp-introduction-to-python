002fc4eec239baa36117b39f9e5d779e1f00db6a
Dilshan Deshapriya Perera <dilshvn@gmail.com>
2022-11-08 15:05:37 +0530

    Add files via upload

---
+++ codes/Numpy array (test).py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr = np.array([[2,3,4],[1,2,3],[3,4,5]]) #2d array
+#print(arr.shape)
+#print(arr[0])
+print(arr)
---
+++ codes/list (test).py
@@ -0,0 +1,6 @@
+import numpy as np
+
+list = [[2,3,4],[1,2,3]]
+#print(list[0,0])
+list = np.array(list)
+print(list[0,0])
